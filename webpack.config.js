ec7745871cac667fb2b567ef9d270165b6360c76
awaqum <bobbysukaanderson@gmail.com>
2023-08-27 13:54:13 +0300

    Commit

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+
+
+let path = require('path');
+const { webpack } = require('webpack');
+
+module.exports = {
+  mode: 'development',
+  entry: './js/script.js',
+  output: {
+    filename: 'bundle.js',
+    path: __dirname + '/js'
+  },
+  watch: true,
+
+  devtool: "source-map",
+
+  module: {}
+};
